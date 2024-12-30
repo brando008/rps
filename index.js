54dfff8de35a8cec7895f722350a40d4a93238f7
brando <192037903+brando008@users.noreply.github.com>
2024-12-30 11:57:02 -0600

    Added functions getHumanChoice & getComputerChoice
    getComuterChoice randomizes a number and is valued
    in a range for rock, paper, or scissors.
    
    getHumanChoice asks for input "r, p, s" and
    expands the word into rock, paper, or scissors.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello");
+console.log("Starting...");
+
+function getComputerChoice() {
+  let randomNumber = Math.random();
+  let computerChoice;
+
+  if (randomNumber <= 0.3333) {
+    computerChoice = "rock";
+  } else if (randomNumber > 0.3333 && randomNumber <= 0.6666) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  return computerChoice;
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt("type in either 'r | p | s': ");
+  switch (humanChoice) {
+    case "r":
+      humanChoice = "rock";
+      break;
+    case "p":
+      humanChoice = "paper";
+      break;
+    case "s":
+      humanChoice = "scissors";
+      break;
+    default:
+      humanChoice = undefined;
+      break;
+  }
+  return humanChoice;
+}
